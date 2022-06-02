d6fbafd94c3832a1a80c3088522ddf747df39bc1
ViniciusTerra <viniterra33@gmail.com>
2022-06-02 20:20:21 -0300

    muda data e pdfs

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,11 @@
-const data = [
-  { frente: "O que é JSX?", verso: "Uma extensão de linguagem do JavaScript" },
-  { frente: "O React é __", verso: "Uma biblioteca JavaScript para construção de interfaces" },
-  { frente: "Componentes devem iniciar com __", verso: "Letra maiúscula" },
-  { frente: "Podemos colocar __ dentro do JSX", verso: "Expressões" },
-];
 
 const allsensors = [
-  { frente: "0001 NA", verso: "Descrição ai do 0001, talvez aonde ele fica oq faz", code:"0001" },
-  { frente: "0002 NF", verso: "Descrição ai do 0002, talvez aonde ele fica oq faz, vou fazer essa descrição maior apenas para teste", code:"0002" },
-  { frente: "0003 NF", verso: "Descrição ai do 0003, talvez aonde ele fica oq faz", code:"0003" },
-  { frente: "0004 NA", verso: "Descrição ai do 0004, talvez aonde ele fica oq faz", code:"0004" },
+  { frente: "FOTOELETRICO", verso: "Sensor Fotoeletrico - E3Z-D81-M1TJ-1 0.3M, YG73440181, Local: 2C8", code:"E3Z-D81-M1TJ-1" },
+  { frente: "INDUTIVO M12", verso: "Sensor Indutivo 12mm - BES M12MG, YG73480265, Local: 2A2", code:"M12-BES-M12MG" },
+  { frente: "LIMITE MECANICO", verso: "Limite Mecanico - WLG2-LDS2, YG73480168, Local: Não Identificado", code:"WLG2LDS2" },
+  { frente: "CAPACITIVO", verso: "Sensor Capacitivo - BCS00MF, YG73480296, Local: 2D3", code:"BCS00MF" },
 ];
+
 
 
 export default allsensors;
